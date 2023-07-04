c889bf6903206e3abfa10be4b551c8adcba4d26e
Abu-zubayer <138626191+Abu-zubayer@users.noreply.github.com>
2023-07-05 03:25:49 +0600

    config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  OWNER_IDS: [], // Bot owner ID's
-  SUPPORT_SERVER: "", // Your bot support server
+  OWNER_IDS: [858966264378294295], // Bot owner ID's
+  SUPPORT_SERVER: "https://discord.gg/eJGKYzqv", // Your bot support server
   PREFIX_COMMANDS: {
     ENABLED: true, // Enable/Disable prefix commands
-    DEFAULT_PREFIX: "!", // Default prefix for the bot
+    DEFAULT_PREFIX: "art", // Default prefix for the bot
   },
   INTERACTIONS: {
     SLASH: false, // Should the interactions be enabled
